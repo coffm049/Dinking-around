04e6209f0ad76d2a549ea7c31b4ce6b7a261ecfb
Christian Coffman <42951304+coffm049@users.noreply.github.com>
2019-05-06 15:01:42 -0400

    Add files via upload

---
+++ Rental/Other_calcs/Caprate.py
@@ -0,0 +1,11 @@
+Rent = 5000
+Expenses = 2920
+Net_mont = Rent - Expenses
+NOI = Net_mont * 12
+Purch_price = 270000
+Rehab = 30000
+Tot_price = Purch_price + Rehab
+
+Cap_rate = (NOI / Tot_price ) * 100
+print(Cap_rate)
+# Want greater than 8%
